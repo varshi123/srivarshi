723f192994a8cd6e7480e76254e49fdc0e62f6ed
varshi123 <varalakshmik.379@gmail.com>
2019-11-08 09:59:46 +0530

    line 2

--- src/test/java/Simple.java
+++ src/test/java/Simple.java
@@ -2,5 +2,8 @@
 public class Simple {
 public static void main(String[] args) {
 	System.out.println("varshi");
+	System.out.println("ashwin");
+	System.out.println("nisha");
+	System.out.println("renu");
 }
 }
